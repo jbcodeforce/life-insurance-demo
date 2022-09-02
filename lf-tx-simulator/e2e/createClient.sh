51b3c96f1efb98accd24af89eab5306a793196e5
jbcodeforce <jerome.boyer@gmail.com>
2022-09-02 13:35:50 -0700

    update

--- lf-tx-simulator/e2e/createClient.sh
+++ lf-tx-simulator/e2e/createClient.sh
@@ -1,1 +1,2 @@
-curl -v -X POST http://localhost:8080/api/v1/clients    -H 'Content-Type: application/json' -d@./client-bob.json
+
+curl -v -X POST $MS_URL/api/v1/clients    -H 'Content-Type: application/json' -d@./client-bob.json
